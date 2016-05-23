135778ab5d77a596592aef1525b34d948b9f8f25
Ismaïl Senhaji <ismail.senhaji@gmail.com>
2016-05-23 16:01:48 +0200

    Migrate :desktop and :headless launchers to Kotlin

---
+++ desktop/src/main/kotlin/io/spacepiano/desktop/DesktopLauncher.kt
@@ -0,0 +1,24 @@
+package io.spacepiano.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import io.spacepiano.Game
+
+object DesktopLauncher {
+    @JvmStatic fun main(args: Array<String>) {
+        createApplication()
+    }
+
+    private fun createApplication(): LwjglApplication {
+        return LwjglApplication(Game(), defaultConfiguration)
+    }
+
+    private val defaultConfiguration: LwjglApplicationConfiguration
+        get() {
+            val configuration = LwjglApplicationConfiguration()
+            configuration.title = "SpacePiano"
+            configuration.width = 640
+            configuration.height = 480
+            return configuration
+        }
+}
---
+++ headless/src/main/kotlin/io/spacepiano/headless/HeadlessLauncher.kt
@@ -0,0 +1,26 @@
+package io.spacepiano.headless
+
+import com.badlogic.gdx.Application
+import com.badlogic.gdx.backends.headless.HeadlessApplication
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration
+import io.spacepiano.Game
+
+/** Launches the headless application. Can be converted into a utilities project or a server application.  */
+object HeadlessLauncher {
+    @JvmStatic fun main(args: Array<String>) {
+        createApplication()
+    }
+
+    private fun createApplication(): Application {
+        // Note: you can use a custom ApplicationListener implementation for the headless project instead of Game.
+        return HeadlessApplication(Game(), defaultConfiguration)
+    }
+
+    private // When this value is negative, Game#render() is never called.
+    val defaultConfiguration: HeadlessApplicationConfiguration
+        get() {
+            val configuration = HeadlessApplicationConfiguration()
+            configuration.renderInterval = -1f
+            return configuration
+        }
+}
