115f9321f34e63f07d0c940add6fd04f0a50bdb2
Ismaïl Senhaji <ismail.senhaji@gmail.com>
2016-05-23 21:58:15 +0200

    Add resource manager class (R) and LoadingScreen

---
+++ core/src/main/kotlin/io/spacepiano/R.kt
@@ -0,0 +1,25 @@
+package io.spacepiano
+
+import com.badlogic.gdx.assets.AssetDescriptor
+import com.badlogic.gdx.assets.AssetManager
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import kotlin.reflect.KClass
+
+object R : AssetManager() {
+    val SKIN = A("ui/skin.json", Skin::class.java)
+
+    init {
+        loadNow(SKIN)
+    }
+
+    fun loadNow(asset: AssetDescriptor<*>) {
+        load(asset)
+        finishLoadingAsset(asset.fileName)
+    }
+
+    fun loadAll() = R::class.java.declaredMethods
+            .filter { it.name.startsWith("get") and it.returnType.isAssignableFrom(AssetDescriptor::class.java) }
+            .forEach { load(it(R) as AssetDescriptor<*>?) }
+
+    private fun <T> A(fileName: String, type: Class<T>) = AssetDescriptor(fileName, type)
+}
--- core/src/main/kotlin/io/spacepiano/screens/LoadingScreen.kt
+++ core/src/main/kotlin/io/spacepiano/screens/LoadingScreen.kt
@@ -1,4 +1,19 @@
 package io.spacepiano.screens
 
+import io.spacepiano.R
+
 class LoadingScreen : Screen() {
+    init {
+        R.loadAll()
+    }
+
+    override fun render(delta: Float) {
+        super.render(delta)
+
+        if (R.update()) {
+            // Go to next screen
+        }
+
+        // Still loading
+    }
 }
